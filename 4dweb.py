b6794d9eb6eeaebd137be46e59e200a128f255b9
mcarqui <mario.carcamo05@gmail.com>
2023-03-28 22:32:07 -0400

    savess

--- 4dweb.py
+++ 4dweb.py
@@ -40,71 +40,107 @@ def TupFind(L):
 	return [[(t[0],t[1]) for t in tmap if t[0]!=t[1]] for tmap in templist]
 
 def FindPhases(X: np.array,F: np.array) -> np.array:
-	DualityWeb = []
-	TrialityMaps = []
-	PermutationMaps = []
-	Tuplemaps = TupFind(X)
+    DualityWeb = [(X,F)]
+    TrialityMaps = []
 
-	#%%
-	def findpermutations(X1, F1, tmaps):
-		relabels = [(X1,F1)]
-		
-		for tlist in tmaps:
-			Xt,Ft = X1.copy(),F1.copy()
-			for t in tlist:
-				Xt, Ft = Swap(Xt, t), Swap(Ft, t)
-			relabels += [(Xt,Ft)]
-		return relabels
-	#%%
+    def equivalent(X1, F1, X2, F2,counter=False):
+        if np.array_equal(X1,X2) and np.array_equal(F1,F2):
+            return True
 
-	def inweb(xt,ft,pmaps,findn=False):
-		for d,testphase in enumerate(pmaps):
-			if np.sum(xt)==np.sum(testphase[0][0]) and np.sum(ft)==np.sum(testphase[0][1]):
-				test = True
-				if len(testphase) > 10000:
-					X1,F1,X2,F2 = xt,ft, testphase[0][0],testphase[0][1]
-					ANodes = [(sorted(X1[node]),sorted(np.transpose(X1)[node]),sorted((F1[node]))) for node in range(len(X1))]
-					BNodes = [(sorted(X2[node]),sorted(np.transpose(X2)[node]),sorted((F2[node]))) for node in range(len(X1))]
+        ANodes = [(sorted(X1[node]),sorted(np.transpose(X1)[node]),sorted(F1[node])) for node in range(len(X1))]
+        BNodes = [(sorted(X2[node]),sorted(np.transpose(X2)[node]),sorted(F2[node])) for node in range(len(X1))]
 
-					for node in ANodes:
-						if node not in BNodes:
-							test = False
+        try:
+            Xt, Ft = X1.copy(), F1.copy()
+            for i in range(len(X1)):
+                if BNodes[i] != ANodes[i]:
+                    b = BNodes[i]
+                    j = ANodes[i:].index(b)+i
+                    Xt,Ft = Swap(Xt,(j,i)),Swap(Ft,(j,i))
+                    ANodes = [(sorted(Xt[node]),sorted(np.transpose(Xt)[node]),sorted(Ft[node])) for node in range(len(X1))]
+        except:
+            return False 
 
-				if test:
-					for phase in testphase:
-						if np.array_equal(xt,phase[0]) and np.array_equal(ft,phase[1]):
-							if findn:
-								return d
-							return True
-		return False
+        if np.array_equal(Xt,X2) and np.array_equal(Ft,F2):
+            return True
+        
+        #final test
+        Aswaps = {}
+        ANodes = [(sorted(Xt[node]),sorted(np.transpose(Xt)[node]),sorted(Ft[node])) for node in range(len(X1))]
+        for i,a in enumerate(ANodes):
+            for j,b in enumerate(ANodes):
+                if a==b and i!=j:
+                    if str(a) not in Aswaps:
+                        Aswaps[str(a)] = [i,i,j]
+                    if i not in Aswaps[str(a)]:
+                        Aswaps[str(a)] += [i]
+                    if j not in Aswaps[str(a)]:
+                        Aswaps[str(a)] += [j]
 
-	qweb = [(X, F)]
-	PermutationMaps += [findpermutations(X,F,Tuplemaps)]
+        temptylist = [val for val in Aswaps.values()]
+        temptlist = [] 
+        for tem in temptylist:
+            mapto = TupFind(np.array(tem))
+            tp = [] 
+            for mapper in mapto:
+                tp += [[[tem[m[0]],tem[m[1]]] for m in mapper]]
+            temptlist += [tp]
 
-	count = 0
+        for tlist in itertools.product(*temptlist):
+            Xp,Fp = Xt.copy(),Ft.copy()
+            for tlistswap in itertools.permutations(tlist):
+                for titer in tlistswap: 
+                    for step in titer:
+                        t = (step[0],step[1])
+                        Xp,Fp = Swap(Xp,t),Swap(Fp,t)
+                        if np.array_equal(Xp,X2) and np.array_equal(Fp,F2):
+                            return True
+        return False
 
-	while len(qweb)!=0:
-		phase = qweb[0]
-		qweb = qweb[1:]
+    def inweb(xt, ft, dweb, findn=False):
+        for d,phase in enumerate(dweb):
+            e1 = equivalent(xt,ft,phase[0],phase[1])
+            e2 = equivalent(np.transpose(xt),ft,phase[0],phase[1])
+            if e1 or e2:# or e3 or e4:
+                if findn:
+                    return d
+                return True
+        return False
+    
+    def anomalies(x1,f1):
+        for i in range(len(x1)):
+            chirals = np.sum(x1[i]) + np.sum(np.transpose(x1)[i])
+            fermis = np.sum(f1[i])
+            if chirals - fermis !=2:
+                return True
+        return False
 
-		Xt,Ft = phase[0].copy(),phase[1].copy()
-		DualityWeb += [(Xt,Ft)]
-		print('Dweb Length: '+str(len(DualityWeb)) + '| Qweb Length: '+str(len(qweb)),end="\r")
-		
-		for n in range(len(Xt)):
-			if np.sum(np.transpose(Xt)[n]) + np.sum(Xt[n])==4:
-				Xi,Fi = Sduality(Xt, Ft, n)
-				count += 1
-				TrialityMaps += [[(Xt,Ft),(Xi,Fi),n+1]]
-				if not inweb(Xi,Fi,PermutationMaps) and not inweb(np.transpose(Xi),Fi,PermutationMaps):
-					PermutationMaps += [findpermutations(Xi,Fi,Tuplemaps)]
-					qweb += [(Xi,Fi)]
-					
+    for phase in DualityWeb:
+        Xt,Ft = phase[0].copy(),phase[1].copy()
 
-	TrialityTuples = [(inweb(t[0][0],t[0][1],PermutationMaps,findn=True),
-		inweb(t[1][0],t[1][1],PermutationMaps,findn=True)) for t in TrialityMaps]
-	print('\n'+str(len(DualityWeb))+' Phase\n')
-	return DualityWeb,TrialityMaps,TrialityTuples
+        print('Dweb Length: '+str(len(DualityWeb)),end="\r")
+
+        if len(DualityWeb)>100:
+            break
+        
+        # if anomalies(Xt,Ft):
+        #     print('Anomaly Found')
+        #     break
+
+        for n in range(len(Xt)):
+            if np.sum(np.transpose(Xt)[n])+np.sum(Xt[n])==4:
+                Xi,Fi = Sduality(Xt, Ft, n)
+                if np.trace(Xi)==0 and np.trace(Fi)==0:
+                    TrialityMaps += [[(Xt,Ft),(Xi,Fi),n+1]]
+                    if not inweb(Xi,Fi,DualityWeb):
+                        DualityWeb += [(Xi,Fi)]
+
+        
+
+    TrialityTuples = [(inweb(t[0][0],t[0][1],DualityWeb,findn=True),
+        inweb(t[1][0],t[1][1],DualityWeb,findn=True)) for t in TrialityMaps]
+    print('\n'+str(len(DualityWeb))+' Phase\n')
+    return DualityWeb,TrialityMaps,TrialityTuples
 
 wp4b = '''X1 2X2 5X5 1 + X1 3X3 4X4 1 + X1 4X4 7X7 1 + X2 4X4 5X5 2 + X3 5X5 6X6 3
 − X1 2X2 4X4 1 − X1 3X3 7X7 1 − X1 4X4 5X5 1 − X2 3X3 5X5 2 − X2 5X5 6X6 2
